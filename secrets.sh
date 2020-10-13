8bda78b2d52fdbb2bded1f5ccff551202f8c61c1
codecakes <akulmat@protonmail.com>
2020-10-13 15:01:03 +0530

    MYC-90 #comment no hardcoded password like vars

--- secrets.sh
+++ secrets.sh
@@ -2,8 +2,9 @@
 
 touch .env
 export SECRET_KEY=$(gcloud secrets versions access latest --secret="SECRET_KEY")
-echo "DB_PORT=5432" >> .env
+echo "DB_PORT=5432" > .env
 echo "SOCIAL_AUTH_TRAILING_SLASH=False  # Remove trailing slash from routes" >> .env
 echo "SOCIAL_AUTH_AUTH0_DOMAIN=$(gcloud secrets versions access latest --secret='SOCIAL_AUTH_AUTH0_DOMAIN')" >> .env
 echo "SOCIAL_AUTH_AUTH0_KEY=$(gcloud secrets versions access latest --secret='SOCIAL_AUTH_AUTH0_KEY')" >> .env
 echo "SOCIAL_AUTH_AUTH0_SECRET=$(gcloud secrets versions access latest --secret='SOCIAL_AUTH_AUTH0_SECRET')" >> .env
+echo "ACCESS_TOKEN_METHOD=POST" >> .env
