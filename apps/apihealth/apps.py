17c0e5c69d442270b49e93bf061717731af3cde3
codecakes <akulmat@protonmail.com>
2020-10-16 04:01:36 +0530

    cleaned files and refactored to return rest response.

--- apps/apihealth/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class APIHealthConfig(AppConfig):
-    """
-    App configuration for api health
-    """
-
-    name = "apps.apihealth"
--- apps/apihealth/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-import apps.apihealth.views as views
-
-urlpatterns = [
-    path("", view=views.api_health, name="index"),
-]
