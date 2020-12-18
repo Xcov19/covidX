89f596652043fe3dd9f534eede6fc26ff50c38ca
Eli <elijahahianyo@gmail.com>
2020-12-18 11:07:54 +0000

    added custom fields to user model

--- apps/auth_zero/models.py
+++ apps/auth_zero/models.py
@@ -29,7 +29,8 @@ class User(AbstractUser):
     default_user_type = UserTypes.PATIENT
     phone_regex = RegexValidator(
         regex=r"^\+?1?\d{9,15}$",
-        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.",
+        message="""Phone number must be entered in the format:
+         '+999999999'. Up to 15 digits allowed.""",
     )
     mobile = models.CharField(validators=[phone_regex], max_length=17)
     mobile_is_verified = models.BooleanField(default=False)
