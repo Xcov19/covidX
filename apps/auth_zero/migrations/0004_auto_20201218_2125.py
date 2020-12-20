4da3fd889912de72353fd3e01bddd7b8e32c5bdd
Eli <elijahahianyo@gmail.com>
2020-12-20 20:08:24 +0000

    MYC-180 #comment corrected issue with line too long

--- apps/auth_zero/migrations/0004_auto_20201218_2125.py
+++ apps/auth_zero/migrations/0004_auto_20201218_2125.py
@@ -7,24 +7,26 @@ import phonenumber_field.modelfields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth_zero', '0003_auto_20201211_1219'),
+        ("auth_zero", "0003_auto_20201211_1219"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='last_otp',
+            model_name="user",
+            name="last_otp",
             field=models.CharField(max_length=20, null=True),
         ),
         migrations.AddField(
-            model_name='user',
-            name='mobile',
-            field=phonenumber_field.modelfields.PhoneNumberField(default=1, max_length=128, region=None),
+            model_name="user",
+            name="mobile",
+            field=phonenumber_field.modelfields.PhoneNumberField(
+                default=1, max_length=128, region=None
+            ),
             preserve_default=False,
         ),
         migrations.AddField(
-            model_name='user',
-            name='otp_verified',
+            model_name="user",
+            name="otp_verified",
             field=models.BooleanField(default=False),
         ),
     ]
