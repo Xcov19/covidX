db769aca8a8897f8b715cf04674b4c53c05dc757
milan-tom <milant076@gmail.com>
2020-12-29 12:28:52 +0000

    MYC-184 #comment Updated test for unverified user

--- apps/auth_zero/tests.py
+++ apps/auth_zero/tests.py
@@ -1,23 +1,16 @@
 from django.test import TestCase
-from apps.auth_zero.models import User
-import logging
+from django.contrib.auth import get_user_model
+
 
 class UserTestCase(TestCase):
     def setUp(self):
-        pass
+        self.user_model = get_user_model()
 
-    def test_unverified_users(self):
-        # unverified_user = User.objects.create_user("username", email="example@mail.com", password="password")
-        unverified_user = User.objects.create(otp_verified=False, email_verified=False)
+    def test_unverified_user(self):
+        unverified_user = self.user_model.objects.create_user(
+            username="unverifiedUser",
+            email="unverifiedUser@mail.com",
+            password="unverifiedUserPassword")
+        self.assertFalse(unverified_user.otp_verified)
+        self.assertFalse(unverified_user.email_verified)
         self.assertFalse(unverified_user.is_verified)
-    
-    def test_partially_verfied_users(self):
-        only_otp_verified_user = User.objects.create(otp_verified=True, email_verified=False)
-        self.assertFalse(only_otp_verified_user.is_verified)
-        
-        only_email_verified_user = User.objects.create(otp_verified=False, email_verified=True)
-        self.assertFalse(only_email_verified_user.is_verified)
-    
-    def test_verified_users(self):
-        verified_user = User.objects.create(otp_verified=True, email_verified=True)
-        self.assertTrue(unverified_user.is_verified)
