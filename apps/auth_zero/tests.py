bcf643c59e6c62d826e3edd6cfaa7ac2446bbcdd
milan-tom <milant076@gmail.com>
2021-01-02 15:43:17 +0000

    MYC-184 #comment Added class to obtain credentials from .ini file

--- apps/auth_zero/tests.py
+++ apps/auth_zero/tests.py
@@ -1,16 +1,56 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.utils.encoding import force_text
 from configparser import ConfigParser
+from dataclasses import dataclass
+from typing import TypeVar
+
+
+@dataclass(frozen=True)
+class Credentials:
+    user: str
+    password: str
+    email: str
+
+
+class CredentialsFactoryLoader:
+    def __init__(self, config_file, credentials_class: TypeVar("Credentials") = None, loader_class=None):
+        self.credentials_class = credentials_class
+        self.loader_class = loader_class(config_file)
+
+    def read_config_for_verification_status(self, verification_status: str) -> dict:
+        return self.loader_class.read_config_for_verification_status(verification_status)
+
+
+class ConfigFileLoader:
+    def __init__(self, config_file: str):
+        self.config = ConfigParser()
+        self.config.read(config_file)
+
+    def read_config_for_verification_status(self, verification_status: str) -> dict:
+        config_settings_for_verification_type = self.config[verification_status]
+        return dict(
+            username=force_text(config_settings_for_verification_type["username"]),
+            password=force_text(config_settings_for_verification_type["password"]),
+            email=force_text(config_settings_for_verification_type["email"]),
+            otp_verified=config_settings_for_verification_type.getboolean("otp_verified"),
+            email_verified=config_settings_for_verification_type.getboolean("email_verified")
+        )
 
 
 class UserTestCase(TestCase):
     def setUp(self):
         self.user_model = get_user_model()
-        self.confg = ConfigParser()
-        self.confg.read("apps/auth_zero/config/test_config.ini")
+        config_file = "apps/auth_zero/config/test_config.ini"
+        self.creds_obj = CredentialsFactoryLoader(config_file, Credentials, ConfigFileLoader)
         self.users = {
-            verification_status: self.user_model.objects.create_user(**dict(self.confg[verification_status]))
-            for verification_status in self.confg.sections()
+            verification_status: self.user_model.objects.create_user(**self.creds_obj.read_config_for_verification_status(verification_status))
+            for verification_status in [
+                "fully_unverified",
+                "only_otp_verified",
+                "only_email_verified",
+                "fully_verified",
+            ]
         }
 
     def test_fully_unverified_user(self):
