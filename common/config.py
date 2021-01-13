454e6f827109feded9d9702e30a95dd3924eaa11
milan-tom <milant076@gmail.com>
2021-01-13 18:51:06 +0000

    MYC-184 #comment Added initial ideas for bytestream config file loader

--- common/config.py
+++ common/config.py
@@ -2,7 +2,7 @@ from django.utils.encoding import force_text
 from configparser import ConfigParser
 from dataclasses import dataclass
 from typing import TypeVar
-
+from io import BytesIO
 
 @dataclass(frozen=True)
 class Credentials:
@@ -20,17 +20,33 @@ class CredentialsFactoryLoader:
         return self.loader_class.read_config(verification_status)
 
 
+class ConfigByteLoader():
+    def __init__(self, config_file: str):
+        with open(config_file, "rb") as f:
+            self.byte_stream = BytesIO(f.read())
+
+    def read_config(self, verification_status: str) -> dict:
+        # config_settings_for_verification_status = not sure how to do
+        return dict(
+            username=force_text(config_settings_for_verification_status["username"]),
+            password=force_text(config_settings_for_verification_status["password"]),
+            email=force_text(config_settings_for_verification_status["email"]),
+            otp_verified=config_settings_for_verification_status.getboolean("otp_verified"),
+            email_verified=config_settings_for_verification_status.getboolean("email_verified")
+        )
+        
+
 class ConfigFileLoader:
     def __init__(self, config_file: str):
         self.config = ConfigParser()
         self.config.read(config_file)
 
     def read_config(self, verification_status: str) -> dict:
-        config_settings_for_verification_type = self.config[verification_status]
+        config_settings_for_verification_status = self.config[verification_status]
         return dict(
-            username=force_text(config_settings_for_verification_type["username"]),
-            password=force_text(config_settings_for_verification_type["password"]),
-            email=force_text(config_settings_for_verification_type["email"]),
-            otp_verified=config_settings_for_verification_type.getboolean("otp_verified"),
-            email_verified=config_settings_for_verification_type.getboolean("email_verified")
+            username=force_text(config_settings_for_verification_status["username"]),
+            password=force_text(config_settings_for_verification_status["password"]),
+            email=force_text(config_settings_for_verification_status["email"]),
+            otp_verified=config_settings_for_verification_status.getboolean("otp_verified"),
+            email_verified=config_settings_for_verification_status.getboolean("email_verified")
         )
