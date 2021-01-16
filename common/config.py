f803dc284debb3def98d7c4cdbab224abdbc19f6
milan-tom <milant076@gmail.com>
2021-01-16 18:54:10 +0000

    MYC-184 #comment Removed ConfigByteLoader, saving it for future pr

--- common/config.py
+++ common/config.py
@@ -29,26 +29,6 @@ class CredentialsLoader:
         return self.loader_class.read_config(verification_status)
 
 
-class ConfigByteLoader:
-    """Loads data from byte stream input"""
-
-    def __init__(self, config_file: str):
-        """Reads byte stream input as binary file."""
-        with open(config_file, "rb") as f:
-            self.byte_stream = BytesIO(f.read())
-
-    def read_config(self, verification_status: str) -> dict:
-        pass
-        # config_settings = not sure how to do
-        # return dict(
-        #     username=force_text(config_settings["username"]),
-        #     password=force_text(config_settings["password"]),
-        #     email=force_text(config_settings["email"]),
-        #     otp_verified=config_settings.getboolean("otp_verified"),
-        #     email_verified=config_settings.getboolean("email_verified")
-        # )
-
-
 class ConfigFileLoader:
     """Loads data from .ini file"""
 
