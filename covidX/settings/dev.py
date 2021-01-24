50bba2727d3d54b0debc45a09ce685ca0400fb02
codecakes <akulmat@protonmail.com>
2021-01-25 01:05:55 +0530

    breaking settings modularly

---
+++ covidX/settings/dev.py
@@ -0,0 +1,15 @@
+import os
+
+from base import *
+
+CONFIG_FILE = "apps/auth_zero/config/test_config.ini"
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "HOST": os.getenv("POSTGRES_DB_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_DB_PORT", "5432"),
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+    },
+}
