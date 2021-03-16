9c2c1b5d36e1408bd0f54e8b89071489dc6291eb
codecakes <akulmat@protonmail.com>
2021-03-16 12:28:21 +0530

    changed base docker image to run genstatic

--- gen_static.sh
+++ gen_static.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python -m pip install -r requirements.txt;
+python3 -m pip install -r requirements.txt;
 SECRET_KEY=$(gcloud secrets versions access latest --secret='SECRET_KEY') python manage.py collectstatic;
